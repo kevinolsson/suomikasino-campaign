b6d7cbbda27e2b5182bc5cbc2232c28a89e97d21
Kevin Olsson <kevinolsson305@gmail.com>
2016-12-21 05:24:48 +0800

    added navigation

--- web/app/themes/comeon/templates/header.php
+++ web/app/themes/comeon/templates/header.php
@@ -1,13 +1,12 @@
-<!-- <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+ <header class="navigation">
+   <a href="<?= esc_url(home_url('/')); ?>" class="navigation__logo">
+      <img alt="<?php bloginfo('name'); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/logo@2x.png"/>
+   </a>
+   <div class="navigation__menu">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation__menu--ul']);
       endif;
       ?>
-    </nav>
-  </div>
-</header>
- -->
+   </div>
+ </header>
