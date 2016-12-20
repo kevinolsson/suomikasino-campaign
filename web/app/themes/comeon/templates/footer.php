8a065bdd835337bb93e4ab793eebf938ec30e0a7
Kevin Olsson <kevinolsson305@gmail.com>
2016-12-21 04:26:29 +0800

    added highlighted portion of footer

--- web/app/themes/comeon/templates/footer.php
+++ web/app/themes/comeon/templates/footer.php
@@ -38,7 +38,11 @@
 			</div>	
 		</div>
 		<div class="footer__row --highlighted">
-			
+			<div class="footer__icon --mga"></div>
+			<div class="footer__icon --license"></div>
+			<div class="footer__fineprint">
+				Suomikasino.com operoi Co-Gaming Ltd (yhtiönumero C47444) yhtiön ja sen lisenssien alaisuudessa, sekä EU valtio Maltan lainsäädännön alla. Yhtiön osoite on Forth Mansions, Ground Floor & 1st Floor, Ta’ Xbiex Sea Front, Ta’ Xbiex, XBX 1027, Malta. Co-Gaming Ltd operoi seuraavien lisenssien alaisina ja toimii niiden sääntöjen mukaisesti: MGA/CL1/619/2009; MGA/CL1/773/2011; MGA/CL1/964/2014; MGA/CL1/877/2013; MGA/CL1/1058/2014; MGA/CL1/1057/2014; MGA/CL1/1094/2015 ja MGA/CL1/1095/2015 ja nämä on myönnetty Maltan peliviranomaiselta The Malta Gaming Authority (MGA), Maltan ainoalta peliviranomaiselta. Tutustu MGA:n toimintaan osoitteessa www.mga.org.mt. Games OS Casino, Playtech Casino ja NolimitCity Casino ovat lisensoituja ja valvottuja Curacaon valtion toimesta. Rahapelaaminen voi aiheuttaa riippuvuutta. Pelaa vastuullisesti.
+			</div>
 		</div>
 	</div>
 </footer>
