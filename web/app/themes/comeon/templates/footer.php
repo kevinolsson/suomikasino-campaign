9477c9a9632e7187cba24d3b21b2f658a13f5233
Kevin Olsson <kevinolsson305@gmail.com>
2016-12-21 03:35:12 +0800

    started work on footer

--- web/app/themes/comeon/templates/footer.php
+++ web/app/themes/comeon/templates/footer.php
@@ -1,5 +1,32 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer class="--shadow">
+	<div class="footer__wrapper">
+		<div class="footer__row">
+			
+		<hr/>		
+		</div>
+		
+		<div class="footer__row">
+			<div class="footer__left">
+				<ul>
+					<li><a href="#">Kasino</a></li>
+					<li><a href="#">Tarjoukset</a></li>
+					<li><a href="#">Tietoa meistä</a></li>
+				</ul>
+				<ul>
+					<li><a href="#">Vastuullinen pelaaminen</a></li>
+					<li><a href="#">Ehdot</a></li>
+					<li><a href="#">Rekisteriseloste</a></li>
+				</ul>
+			</div>
+			<div class="footer__right">
+				<strong>Suomikasino</strong>
+				<p>
+					Suomikasino avattiin vuonna 2013 ja siitä on nopeasti tullut suomalaisten suosikkikasino, jonka päämääränä on luoda yksi sivu parhaille kasinopeleille. Suomikasinolla pelaaminen on helppoa, hauskaa ja turvallista. Pelaamaan pääset myös mobiilisti älypuhelintasi käyttäen. Suomikasino on nettikasino, joka tarjoaa suuremman pelivalikoiman kuin mikään muu suomalainen kasinosivusto tai mobiilikasino.
+				</p>
+			</div>	
+		</div>
+		<div class="footer__row --highlighted">
+			
+		</div>
+	</div>
 </footer>
