1bbe55f5954e61e8ad496bc9c8c9e76e4d9e089a
Kevin Olsson <kevinolsson305@gmail.com>
2016-12-24 04:18:01 +0800

    changed footer scss to BEM

--- web/app/themes/comeon/templates/footer.php
+++ web/app/themes/comeon/templates/footer.php
@@ -1,8 +1,8 @@
-<footer class="--shadow">
+<footer class="footer footer--shadow">
 	<div class="footer__wrapper">
-		<div class="footer__row --right">
-			<a href="https://www.suomikasino.com/tietoa-meista/"><div class="footer__icon --eu"></div></a>
-			<a href="#"><div class="footer__icon --age"></div></a>
+		<div class="footer__row right-align">
+			<a href="https://www.suomikasino.com/tietoa-meista/"><div class="footer__icon footer__icon--eu"></div></a>
+			<a href="#"><div class="footer__icon footer__icon--age"></div></a>
 		<hr/>		
 		</div>
 		
@@ -20,10 +20,10 @@
 				</ul>
 				<div class="footer__left--bottom">
 					<span>Seuraa meitä</span><br/>
-					<a href="https://www.facebook.com/suomikasinocom"><div class="footer__icon --facebook"></div></a>
-					<a href="https://twitter.com/suomikasinocom"><div class="footer__icon --twitter"></div></a>
-					<div class="footer__icon --no-spr">
-						<a href="http://earnaffiliates.com/"><div class="footer__icon --earn"></div></a>
+					<a href="https://www.facebook.com/suomikasinocom"><div class="footer__icon footer__icon--facebook"></div></a>
+					<a href="https://twitter.com/suomikasinocom"><div class="footer__icon footer__icon--twitter"></div></a>
+					<div class="footer__icon footer__icon--no-spr">
+						<a href="http://earnaffiliates.com/"><div class="footer__icon footer__icon--earn"></div></a>
 						<br/><span>Affiliates Earn</span>
 					</div>
 
@@ -37,9 +37,9 @@
 				</p>
 			</div>	
 		</div>
-		<div class="footer__row --highlighted">
-			<div class="footer__icon --mga"></div>
-			<div class="footer__icon --license"></div>
+		<div class="footer__row footer__row--highlighted">
+			<div class="footer__icon footer__icon--mga"></div>
+			<div class="footer__icon footer__icon--license"></div>
 			<div class="footer__fineprint">
 				Suomikasino.com operoi Co-Gaming Ltd (yhtiönumero C47444) yhtiön ja sen lisenssien alaisuudessa, sekä EU valtio Maltan lainsäädännön alla. Yhtiön osoite on Forth Mansions, Ground Floor & 1st Floor, Ta’ Xbiex Sea Front, Ta’ Xbiex, XBX 1027, Malta. Co-Gaming Ltd operoi seuraavien lisenssien alaisina ja toimii niiden sääntöjen mukaisesti: MGA/CL1/619/2009; MGA/CL1/773/2011; MGA/CL1/964/2014; MGA/CL1/877/2013; MGA/CL1/1058/2014; MGA/CL1/1057/2014; MGA/CL1/1094/2015 ja MGA/CL1/1095/2015 ja nämä on myönnetty Maltan peliviranomaiselta The Malta Gaming Authority (MGA), Maltan ainoalta peliviranomaiselta. Tutustu MGA:n toimintaan osoitteessa www.mga.org.mt. Games OS Casino, Playtech Casino ja NolimitCity Casino ovat lisensoituja ja valvottuja Curacaon valtion toimesta. Rahapelaaminen voi aiheuttaa riippuvuutta. Pelaa vastuullisesti.
 			</div>
